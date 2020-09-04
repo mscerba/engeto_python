b9207198d21b18d23d5f5c25c91b4b035f263180
Michal Ščerba <scerba4@gmail.com>
2020-09-04 21:37:30 +0200

    Column Widths

---
+++ cv_73.py
@@ -0,0 +1,23 @@
+LISTS = [['ame', 'Item', 'Amount', 'Unit_Price', 'Total_Price'],
+['Bettison, Elnora', 'Doxycycline Hyclate', 98, 23.43, 2296.14],
+['McShee, Glenn', 'DROXIA', 27, 33.86, 914.22],
+['Conyard, Phil', 'Nadolol', 44, 12.35, 543.4],
+['Bettison, Elnora', 'Claravis', 91, 9.85, 896.35],
+['Idalia, Craig', 'Nadolol', 83, 12.35, 1025.05],
+['Woodison, Annie', 'Metolazone', 46, 43.06, 1980.76],
+['Woodison, Annie', 'DROXIA', 50, 33.86, 1693.0],
+['Skupinski, Wilbert', 'Nadolol', 60, 12.35, 741.0],
+['Conyard, Phil', 'WRINKLESS PLUS', 49, 23.55, 1153.95],
+['Bettison, Elnora', 'Doxycycline Hyclate', 59, 23.43, 1382.37],
+['Skupinski, Wilbert', 'Metolazone', 51, 43.06, 2196.06],
+['McShee, Glenn', 'Claravis', 1, 9.85, 9.85]]
+
+
+def get_widths():
+    for col in LISTS:
+        dict_vy = {}
+        for i in col:
+            delka = len(i)
+            if
+
+get_widths()
